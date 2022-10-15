2bacf632bf2f09ac194510140b8a7bfbb0fbf09b
Robert Aiken <robertsiraaiken@gmail.com>
2022-10-15 14:35:10 -0500

    Refactor functions

--- src/restaurant.js
+++ src/restaurant.js
@@ -13,21 +13,19 @@ return restaurant
 
 //addMenuItem
 function addMenuItem(restaurant, menuItem){
-  if (menuItem.type === "lunch" && restaurant.menus.lunch.includes(menuItem) === false) {
-    restaurant.menus.lunch.push(menuItem)
-  } else if (menuItem.type === "breakfast" && restaurant.menus.breakfast.includes(menuItem) === false) {
-    restaurant.menus.breakfast.push(menuItem)
-  } else if (menuItem.type === "dinner" && restaurant.menus.dinner.includes(menuItem) === false){
-    restaurant.menus.dinner.push(menuItem)
+var menuItemType = menuItem.type
+  if (restaurant.menus[menuItemType].includes(menuItem) === false){
+    restaurant.menus[menuItemType].push(menuItem)
   }
-return restaurant
+  return restaurant
 }
+
 
 var pizzaRestaurant = createRestaurant("Sexy Pizza");
 var bbqPizza = {
   name: "BBQ Chicken Pizza",
   price: "12.49",
-  type: "lunch"
+  type: "breakfast"
 };
 
 var baconEggsPizza = {
@@ -43,32 +41,27 @@ addMenuItem(pizzaRestaurant, baconEggsPizza);
 //console.log(pizzaRestaurant)
 
 //removeMenuItem
-function removeMenuItem(restaurant, menuItem, itemType){
 
-if (restaurant.menus.breakfast[0] === undefined){
-  return `Sorry, we don't sell ${menuItem}, try adding a new recipe!`
-}
-  else if (itemType === "breakfast" && menuItem === restaurant.menus.breakfast[0].name) {
-    restaurant.menus.breakfast.pop()
-    return `No one is eating our ${menuItem} - it has been removed from the ${itemType} menu!`
+function removeMenuItem(restaurant, menuItem, itemType){
+  if (restaurant.menus[itemType] === undefined){
+     return `Sorry, we don't sell ${menuItem}, try adding a new recipe!`
   }
-    else if (itemType === "lunch" && menuItem === restaurant.menus.lunch[0].name){
-      restaurant.menus.lunch.pop()
+  for ( i = 0; i<restaurant.menus[itemType].length; i++ ){
+    if (menuItem === restaurant.menus[itemType][i]["name"]){
+      restaurant.menus[itemType].splice(i,1)
       return `No one is eating our ${menuItem} - it has been removed from the ${itemType} menu!`
     }
-      else if(itemType === "dinner" && menuItem === restaurant.menus.dinner[0].name){
-        restaurant.menus.dinner.pop()
-        return `No one is eating our ${menuItem} - it has been removed from the ${itemType} menu!`
-      }
-
+  }
+  return `Sorry, we don't sell ${menuItem}, try adding a new recipe!`
 }
 
 
-
-console.log(pizzaRestaurant)
-console.log(pizzaRestaurant.menus.breakfast[0].name)
-console.log(removeMenuItem(pizzaRestaurant,"Bacon and Eggs Pizza", "breakfast"))
-console.log(pizzaRestaurant)
+var restaurant = pizzaRestaurant
+var itemType = "breakfast"
+// console.log(pizzaRestaurant)
+// console.log(pizzaRestaurant.menus[itemType])
+// removeMenuItem(pizzaRestaurant, "Bacon and Eggs Pizza", "breakfast")
+// console.log(pizzaRestaurant)
 
 
 
